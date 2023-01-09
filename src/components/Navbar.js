7e58bc5de8b23e6a9828414d143ab177d010fcd2
Wiktor <wiktorzielak@gmail.com>
2023-01-09 23:52:21 +0100

    added responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import logo from "./../favicon.svg"
 import { useHistory } from 'react-router';
 
 
 function Navbar() {
   const history = useHistory();
+  const nav = useRef();
+  const home = useRef();
+  const addSong = useRef();
+
+  const barClick = () => {
+    home.current.classList.toggle('hidden');
+    addSong.current.classList.toggle('hidden');
+    nav.current.classList.toggle('slim');
+  }
+
   return (
-    <nav>
+    <nav ref={nav}>
+      <div className="bar-container" onClick={barClick}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
         <ul>
             <li className="logo" onClick={() => history.push('/')}><img src={logo} alt="No logo" /></li>
-            <li className="home" onClick={() => history.push('/')}>Home</li>
-            <li className="add-song" onClick={() => history.push('/addsong')}>Add song</li>
+            <li className="home" ref={home} onClick={() => history.push('/')}>Home</li>
+            <li className="add-song" ref={addSong} onClick={() => history.push('/addsong')}>Add song</li>
         </ul>
   </nav>
   )
