d15ffbd8df3788f26766d69933dc430f3d277d57
Wiktor <wiktorzielak@gmail.com>
2023-01-09 23:01:11 +0100

    improved add song form

--- src/components/AddSong.js
+++ src/components/AddSong.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState}  from 'react'
 
 function AddSong() {
-  return (
-    <div className="form">
-        
+    const [author, setAuthor] = useState('');
+    const [title, setTitle] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        alert(author)
+    }
+    return (
+        <div className="form-container">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <label htmlFor="author">Author</label>
+                    <input type="text" name="author" id="author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <button type="submit" >Submit</button>
+                </form>
+            </div>
     </div>
-  )
+    )
 }
 
 export default AddSong
