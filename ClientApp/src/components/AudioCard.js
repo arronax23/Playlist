b98f62a5fc842a2ba617049911c5c2231fa33fe5
Wiktor <wiktorzielak@gmail.com>
2023-01-15 23:57:59 +0100

    styled AudioCard

--- ClientApp/src/components/AudioCard.js
+++ ClientApp/src/components/AudioCard.js
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import useFetchGet from './../customHooks/useFetchGet'
 import { useParams } from 'react-router-dom'
 
 function AudioCard() {
     const { id } = useParams();
     const {data : song, isPending, error, httpResposne} = useFetchGet(`/api/GetOneSong/${id}`);
+    
+    const audio = useRef();
+    const playButton = useRef();
+    const pauseButton = useRef();
+    const stopButton = useRef();
+
+    const onPlay = () => {
+        audio.current.play();
+
+        playButton.current.style.display = 'none'
+        pauseButton.current.style.display = 'inline-block'
+        stopButton.current.style.display = 'inline-block'
+    }
+
+    const onPause = () => {
+        audio.current.pause();
+
+        playButton.current.style.display = 'inline-block'
+        pauseButton.current.style.display = 'none'
+        stopButton.current.style.display = 'inline-block'
+    }
+
+    const onStop = () => {
+        audio.current.pause();
+        audio.current.currentTime = 0;
+
+        playButton.current.style.display = 'inline-block'
+        pauseButton.current.style.display = 'none'
+        stopButton.current.style.display = 'none'
+    }
+
+
+    
     return (
         song && 
         (
+        <div className="audio-card-container">
             <div className="audio-card">
-            <div className="image">
-                <img src={`/img/${song.imgPath}`} />
+                <div className="image">
+                    <img src={`/img/${song.imgPath}`} />
+                </div>
+                <div className="author">
+                    {song.author}
+                </div>
+                <div className="title">
+                    {song.title}
+                </div>
+                <div className="audio-buttons">
+                    <i class="fa-solid fa-play" onClick={onPlay} ref={playButton}></i>
+                    <i class="fa-solid fa-pause" onClick={onPause} ref={pauseButton}></i>
+                    <i class="fa-solid fa-stop" onClick={onStop} ref={stopButton}></i>
+                </div>
+                <audio src={`/audio/${song.audioPath}`} ref={audio}></audio>
             </div>
-            <div className="author">
-                {song.author}
-            </div>
-            <div className="title">
-                {song.title}
-            </div>
-            <audio src={`/audio/${song.audioPath}`} autoPlay></audio>
         </div>
         )  
     )
