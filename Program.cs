6ca3f09e11c732be614794a321f98ba8062cd1d5
Wiktor <wiktorzielak@gmail.com>
2023-03-03 23:57:35 +0100

    refactoring

--- Program.cs
+++ Program.cs
@@ -4,19 +4,6 @@ using Playlist.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-//builder.Services.AddControllers()
-//      .ConfigureApiBehaviorOptions(options =>
-//      {
-//          options.InvalidModelStateResponseFactory = context =>
-//          {
-//              var loggerFactory = context.HttpContext.RequestServices
-//                .GetRequiredService<ILoggerFactory>();
-//              var logger = loggerFactory.CreateLogger(context.ActionDescriptor.DisplayName);
-
-
-//              return new BadRequestObjectResult(context.ModelState);
-//          };
-//      }); ;
 builder.Services.AddSingleton<MongoDBService>();
 
 var app = builder.Build();
